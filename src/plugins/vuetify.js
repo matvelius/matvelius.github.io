b98cc5ded7f73b3e4c3b251af418863179d9261c
Matvey <matvelius@gmail.com>
2020-12-07 13:52:31 -0600

    solbe content, frosted glass effect

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,16 @@
 import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import Vuetify from "vuetify/lib/";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  breakpoint: {
+    thresholds: {
+      xs: 460,
+      sm: 750,
+      md: 1264,
+      lg: 1904,
+      xl: 2400
+    },
+  }
+});

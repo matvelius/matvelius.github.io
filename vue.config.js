8f1a8a7486cc40997497b758f3f0fd9902189253
Matvey <matvelius@gmail.com>
2020-12-14 19:59:42 -0600

    another deploy attempt

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  devServer: {
-    "public": "http://localhost:8080"
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio/'
-    : '/'
+  // devServer: {
+  //   "public": "http://localhost:8080"
+  // },
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/portfolio/'
+  //   : '/'
 };
